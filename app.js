8e00b5cfe57d26e600a9a4bf3c802959597af633
jyzib <jazibzaidi02@gmail.com>
2023-09-20 12:10:40 +0530

    server added

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = express()
+const port = 2000
+app.get('/',(req,res)=>{
+   res.status(200).json({success:true})
+})
+app.listen(port,()=>console.log(`Server is running on port no ${port}`))
